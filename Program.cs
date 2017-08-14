07c5c32feaeff98c5ef96c0ca3314857f1861623
Jake LaFond <jakelafond@gmail.com>
2017-08-14 14:52:24 -0400

    changed to operand instead of input

--- Program.cs
+++ Program.cs
@@ -9,28 +9,28 @@ namespace math_operations
         public static double Multiplication(double x, double y) => x * y;
         public static double Division(double x, double y) => x / y;
         public static double Remainder(double x, double y) => x % y;
-        public static void ShowResults(double input1, double input2, double sum, double diff, double mult, double div, double remain)
+        public static void ShowResults(double operand1, double operand2, double sum, double diff, double mult, double div, double remain)
         {
-            Console.WriteLine($"The result of {input1} + {input2} is {sum}");
-            Console.WriteLine($"The difference of {input1} and {input2} is {diff}");
-            Console.WriteLine($"The multiplication of {input1} and {input2} is {mult}");
-            Console.WriteLine($"The division of {input1} and {input2} is {div}");
-            Console.WriteLine($"The remainder of {input1} and {input2} is {remain}");
+            Console.WriteLine($"The result of {operand1} + {operand2} is {sum}");
+            Console.WriteLine($"The difference of {operand1} and {operand2} is {diff}");
+            Console.WriteLine($"The multiplication of {operand1} and {operand2} is {mult}");
+            Console.WriteLine($"The division of {operand1} and {operand2} is {div}");
+            Console.WriteLine($"The remainder of {operand1} and {operand2} is {remain}");
         }
         static void Main(string[] args)
         {
             Console.WriteLine("I AM GOING TO MATH YOU UP. GIVE ME A NUMBER!:");
-            var input1 = double.Parse(Console.ReadLine());
+            var operand1 = double.Parse(Console.ReadLine());
             Console.WriteLine("GIVE ME NUMBER 2!:");
-            var input2 = double.Parse(Console.ReadLine());
+            var operand2 = double.Parse(Console.ReadLine());
 
-            var sum = Sum(input1, input2);
-            var diff = Difference(input1, input2);
-            var mult = Multiplication(input1, input2);
-            var div = Division(input1, input2);
-            var remain = Remainder(input1, input2);
+            var sum = Sum(operand1, operand2);
+            var diff = Difference(operand1, operand2);
+            var mult = Multiplication(operand1, operand2);
+            var div = Division(operand1, operand2);
+            var remain = Remainder(operand1, operand2);
 
-            ShowResults(input1, input2, sum, diff, mult, div, remain);
+            ShowResults(operand1, operand2, sum, diff, mult, div, remain);
         }
     }
 }
